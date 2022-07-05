52eedefb0e528545992bf3fd9397264c79f280e6
AzlanCoding <azlanchenlong@gmail.com>
2022-07-06 06:42:26 +0800

    Tested further

--- asposewordstest.py
+++ asposewordstest.py
@@ -7,3 +7,10 @@ builder = aw.DocumentBuilder(doc)
 builder.writeln("Hello Aspose.Words for Python via .NET")
 
 doc.save("C:\\Temp\\out.docx")
+
+doc = aw.Document()
+builder = aw.DocumentBuilder(doc)
+            
+builder.write("Hello world!")
+
+doc.save("C:\\Temp\\out.pdf")
