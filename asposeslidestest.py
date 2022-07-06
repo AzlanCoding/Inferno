c8cc7579de930fcb1cf0922014fc1d09a4a299c9
AzlanCoding <azlanchenlong@gmail.com>
2022-07-06 17:48:59 +0800

    Create asposeslidestest.py

---
+++ asposeslidestest.py
@@ -0,0 +1,7 @@
+import aspose.slides as slides
+
+# Instantiate a Presentation object that represents a presentation file
+with slides.Presentation() as presentation:
+    slide = presentation.slides[0]
+    slide.shapes.add_auto_shape(slides.ShapeType.LINE, 50, 150, 300, 0)
+    presentation.save("NewPresentation_out.pptx", slides.export.SaveFormat.PPTX)
