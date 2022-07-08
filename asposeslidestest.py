29f18c3d7ba130b8b7c4ee5b9a07f575e9135c91
AzlanCoding <azlanchenlong@gmail.com>
2022-07-09 07:11:06 +0800

    Update asposeslidestest.py

--- asposeslidestest.py
+++ asposeslidestest.py
@@ -5,3 +5,7 @@ with slides.Presentation() as presentation:
     slide = presentation.slides[0]
     slide.shapes.add_auto_shape(slides.ShapeType.LINE, 50, 150, 300, 0)
     presentation.save("NewPresentation_out.pptx", slides.export.SaveFormat.PPTX)
+with slides.Presentation("NewPresentation_out.pptx") as pres:
+    # Printing the total number of slides present in the presentation
+    print(pres.slides.length)
+    print(pres.slides)
